b18df49ced3ad90cdf4f6ea7868e826f75d87c9f
ktykeson <chachingdj@gmail.com>
2024-05-15 00:17:11 +0200

    css changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -85,16 +85,9 @@ const Navbar = () => {
           </li>
         </ul>
         <div className="navbar-actions">
-          <button className="navbar-button">Login</button>
           <button className="navbar-button" onClick={toggleSidebar}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="shopping-cart-icon"
-              viewBox="0 0 24 24"
-            >
-              <path fill="none" d="M0 0h24v24H0z" />
-              <path d="M16 4H4a2 2 0 0 0-2 2v2h2v13a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8h2V6a2 2 0 0 0-2-2zm-2 13H6v-2h8v2zm4-5H6V9h12v3z" />
-            </svg>
+            <div className="wheel1"></div>
+            <div className="wheel2"></div>
           </button>
         </div>
       </nav>

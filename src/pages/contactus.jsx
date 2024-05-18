7649c931627a4f73dfb954d773e6226a31668f73
Aung Kyaw Zorr <aungkyawzorr@Aungs-MacBook-Air.local>
2024-05-18 23:03:26 +0700

    contact us page design changes

--- src/pages/contactus.jsx
+++ src/pages/contactus.jsx
@@ -7,8 +7,8 @@ const ContactUs = () => {
   return (
     <div className="contact-container">
       <div className="header">
-        <img src="car.jpg" alt="Quest Products" className="header-image" />
-        <h1>CONTACT US</h1>
+        {/* <img src="car.jpg" alt="Quest Products" className="header-image" /> */}
+        <h1 className="main-title">CONTACT US</h1>
       </div>
       <div className="content">
         <div className="left-panel">
@@ -18,41 +18,70 @@ const ContactUs = () => {
             from the Quest community.
           </p>
           <div className="contact-methods">
-            <button className="contact-button whatsapp">
-              <i className="fa fa-whatsapp"></i> Whatsapp
-            </button>
-            <button className="contact-button phone">
-              <i className="fa fa-phone"></i> Phone
-            </button>
-            <button className="contact-button email">
-              <i className="fa fa-instagram"></i> Instagram
-            </button>
+          <a href="https://whatsapp.com/" className="contact-button whatsapp">
+              <img src="social-media-icons/whatsapp.png" alt="WhatsApp Logo" className="contact-icon" /> Whatsapp
+          </a>
+          <a href="https://instagram.com" className="contact-button phone">
+              <img src="social-media-icons/instagram.png" alt="Instagram Logo" className="contact-icon" /> Instagram
+          </a>
+          <a href="mailto:example@example.com" className="contact-button email">
+              <img src="social-media-icons/email.png" alt="Email Logo" className="contact-icon" /> Email
+          </a>
+        </div>
+        <div className="image-section">
+          <img src="exercise.png"></img>
+        </div>
+
+          <div className="extra-methods">
+            <h3>MAIL US</h3>
+            <p>Quest Nutrition, LLC</p>
+            <p>Attn: Consumer Engagement</p>
+            <p>777 S Aviation Blvd. Ste.100</p>
+            <p>El Segundo, CA, 90245</p>
           </div>
         </div>
         <div className="right-panel">
           <h2>MESSAGE US</h2>
-          <p>
-            Select the appropriate subject below and fill out the inquiry form
-            so we can get back to you quickly.
-          </p>
+          
           <form className="contact-form">
+            <div className="form-row">
+              <div className="form-group">
+                <label htmlFor="first-name">First Name: *</label>
+                <input type="text" id="first-name" name="first-name" required></input>
+              </div>
+              <div className="form-group">
+                <label htmlFor="last-name">Last Name:</label>
+                <input type="text" id="last-name" name="last-name"></input>
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-group">
+                <label htmlFor="email">Email: *</label>
+                <input type="email" id="email" name="email" required></input>
+              </div>
+              <div className="form-group">
+                <label htmlFor="phone">Phone:</label>
+                <input type="tel" id="phone" name="phone"></input>
+              </div>
+            </div>
             <div className="form-group">
-              <label htmlFor="subject">Subject:</label>
-              <select id="subject" name="subject">
+              <label htmlFor="subject">Subject: *</label>
+              <select id="subject" name="subject" required>
                 <option value="product">Product Inquiry</option>
                 <option value="support">Support Request</option>
                 <option value="general">General Inquiry</option>
               </select>
             </div>
             <div className="form-group">
-              <label htmlFor="message">Message:</label>
-              <textarea id="message" name="message" rows="4"></textarea>
+              <label htmlFor="message">Message: *</label>
+              <textarea type="text" id="message" name="message" rows="4" required></textarea>
             </div>
             <button type="submit" className="submit-button">
               Send Message
             </button>
           </form>
         </div>
+
       </div>
     </div>
   );

f9914875087f0b90e53ec255997d7812d69bab53
Aung Kyaw Zorr <aungkyawzorr@Aungs-MacBook-Air.local>
2024-05-22 22:15:08 +0700

    Contact Us form functionality

--- src/pages/contactus.jsx
+++ src/pages/contactus.jsx
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/contact/ContactUs.css";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 
 const ContactUs = () => {
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [phone_number, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [subject, setSubject] = useState("");
+  const [submitMessage, setSubmitMessage] = useState("");
+  const endpoint =
+    "https://w6e3ol5nlnx5zov7ed5nmxv7la0felyk.lambda-url.eu-north-1.on.aws/";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = { first_name, last_name, email, phone_number, message, subject };
+
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPhoneNumber("");
+    setMessage("");
+    setSubject("");
+
+    const fetchPromise = fetch(endpoint, {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      body: JSON.stringify(data),
+    });
+    fetchPromise
+      .then((response) => {
+        if (response.ok) {
+          setSubmitMessage("Message successfully sent!");
+        } else {
+          setSubmitMessage("Error encountered while sending message.");
+        }
+        setTimeout(() => {
+          setSubmitMessage("");
+        }, 10000);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setSubmitMessage("Error encountered while sending message.");
+        setTimeout(() => {
+          setSubmitMessage("");
+        }, 10000);
+      });
+  };
   return (
     <div className="contact-container">
       <div className="header">
@@ -38,7 +88,7 @@ const ContactUs = () => {
               Instagram
             </a>
             <a
-              href="mailto:example@example.com"
+              href="mailto:wimnutritionsoporte@gmail.com"
               className="contact-button email"
             >
               <img
@@ -66,7 +116,7 @@ const ContactUs = () => {
         <div className="right-panel">
           <h2>MESSAGE US</h2>
 
-          <form className="contact-form">
+          <form className="contact-form" action={endpoint} onSubmit={handleSubmit} method="POST">
             <div className="form-row">
               <div className="form-group">
                 <label htmlFor="first-name">First Name: *</label>
@@ -74,39 +124,43 @@ const ContactUs = () => {
                   type="text"
                   id="first-name"
                   name="first-name"
+                  value={first_name}
                   required
+                  onChange={(e) => setFirstName(e.target.value)}
                 ></input>
               </div>
               <div className="form-group">
                 <label htmlFor="last-name">Last Name:</label>
-                <input type="text" id="last-name" name="last-name"></input>
+                <input type="text" id="last-name" name="last-name" value={last_name} onChange={(e) => setLastName(e.target.value)}></input>
               </div>
             </div>
             <div className="form-row">
               <div className="form-group">
                 <label htmlFor="email">Email: *</label>
-                <input type="email" id="email" name="email" required></input>
+                <input type="email" id="email" name="email" value={email} required onChange={(e) => setEmail(e.target.value)}></input>
               </div>
               <div className="form-group">
                 <label htmlFor="phone">Phone:</label>
-                <input type="tel" id="phone" name="phone"></input>
+                <input type="tel" id="phone" name="phone" value={phone_number} onChange={(e) => setPhoneNumber(e.target.value)}></input>
               </div>
             </div>
             <div className="form-group">
               <label htmlFor="subject">Subject: *</label>
-              <select id="subject" name="subject" required>
+              <select id="subject" name="subject" value={subject} onChange={(e) => setSubject(e.target.value)} required>
                 <option value="product">Product Inquiry</option>
                 <option value="support">Support Request</option>
                 <option value="general">General Inquiry</option>
               </select>
             </div>
             <div className="form-group">
-              <label htmlFor="message">Message: *</label>
+              <label htmlFor="message" >Message: *</label>
               <textarea
                 type="text"
                 id="message"
                 name="message"
                 rows="4"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
                 required
               ></textarea>
             </div>
@@ -114,6 +168,7 @@ const ContactUs = () => {
               Send Message
             </button>
           </form>
+          {submitMessage && <p>{submitMessage}</p>}
         </div>
       </div>
     </div>
